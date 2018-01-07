9e847aa223c2b7077c4befebbb07ee1b405f88e4
Wollno <robert@wollner.uk>
2018-01-07 16:17:22 +0000

    fix

--- page.js
+++ page.js
@@ -1,6 +1,6 @@
-var base = "https://wollno.github.io/bgv/";
+var base = escape("https://wollno.github.io/bgv/");
 
-var a = escape(window.location.substring(base.length));
+var a = escape(window.location).substring(base.length);
 
 ReactDOM.render(
     <h1>We're at: {a}</h1>,
