e83c223d8607c23d05ddeee25b003cc199ba2fe7
Wollno <robert@wollner.uk>
2018-01-24 22:15:31 +0000

    test header image

--- page_funcs.js
+++ page_funcs.js
@@ -15,6 +15,45 @@ function make_collection(urlStub){
 
     $.get(space_url).done(function(data){
         console.log(data);
+        try {
+            var entry = JSON.parse(data)["items"][0];
+            var title = entry.name;
+            $("title").text(title);
+
+            return class bgvcollection extends RTCIceCandidate.Component {
+                render() {
+                    <div class="page-header section-dark" style="background-image: url('assets/img/antoine-barres.jpg')">
+                        <div class="filter"></div>
+                        <div class="content-center">
+                            <div class="container">
+                                <div class="title-brand">
+                                    <h1 class="presentation-title">{title}</h1>
+                                    <div class="fog-low">
+                                        <img src="assets/img/fog-low.png" alt="">
+                                    </div>
+                                    <div class="fog-low right">
+                                        <img src="assets/img/fog-low.png" alt="">
+                                    </div>
+                                </div>
+
+                                <h2 class="presentation-subtitle text-center">Make your mark with a Free Bootstrap 4 UI Kit! </h2>
+                            </div>
+                        </div>
+                        <div class="moving-clouds" style="background-image: url('assets/img/clouds.png'); ">
+
+                        </div>
+                        <h6 class="category category-absolute">Designed and coded by
+                            <a href="https://www.creative-tim.com" target="_blank">
+                                <img src="assets/img/creative-tim-white-slim2.png" class="creative-tim-logo">
+                            </a>
+                        </h6>
+                    </div>
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
     }).fail(function(e){
         console.log("error");
         console.log(e);
